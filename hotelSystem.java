7257e03a36a00a1d6b61d5d813690f0f20aacb30
hannahalshech <79874128+hannahalshech@users.noreply.github.com>
2022-02-25 09:31:19 -0500

    Add files via upload

--- hotelSystem.java
+++ hotelSystem.java
@@ -251,20 +251,70 @@ public class hotelSystem {
 			return(customers);
 		}
 		
-		System.out.println("Create a password for your account. Passwords must be at least 8 characters: ");
+		System.out.println("Create a password for your account. Passwords must be at least 8 characters, contain at least one capital letter, at least one number, and at least one special character(!,#,$,@,&, or *): ");
 		String password = scan.next();
 		
-		// validate length of password is >= 8
+		// validate length of password is >= 8 characters
 		char passGood = 'n';		
 		if(password.length() >= 8) {
 			passGood = 'y';	
 		}
 		while(passGood != 'y') {
-			System.out.println("Password not long enough! Password must contain 8 characters.");
+			System.out.println("Password not long enough! Password must contain 8 characters.\nEnter new password: ");
 			password = scan.next();
 			if(password.length() > 8) {
 				passGood = 'y';	
-			}			
+			}
+
+        //validate if special characters are included
+        if(password.contains("@") | password.contains("#") | password.contains("!") | password.contains("$") | password.contains("&") | password.contains("*")){
+            passGood= 'y';
+        }
+        while(passGood != 'y'){
+            System.out.println("This is not a valid password! Password must contain at least one of the following special characters: !, #, $, @, &, or *.\nEnter new password: ");
+            password = scan.next();
+            if(password.contains("@") | password.contains("#") | password.contains("!") | password.contains("$") | password.contains("&") | password.contains("*")){
+                passGood = 'y';
+            }
+        }
+
+        //validate length of password <= 50 characters
+        if(password.length() <= 50){
+            passGood = 'y';
+        }
+        while(passGood != 'y'){
+            System.out.println("This password is too long! Password must contain 50 characters or less.\nEnter new password: ");
+            password = scan.next();
+            if(password.length() <= 50){
+                passGood = 'y';
+            }
+        }
+
+        //validate if password contains digit
+        if (password.contains("0") | password.contains("1") | password.contains("2") | password.contains("3") | password.contains("4") | password.contains("5") | password.contains("6") | password.contains("7") | password.contains("8") | password.contains("9")){
+            passGood = 'y';
+        }
+        while (passGood!= 'y'){
+            System.out.println("This password does not contain any digits. Your password must contain at least one digit.\nEnter new password: ");
+            password = scan.next();
+            if(password.contains("0") | password.contains("1") | password.contains("2") | password.contains("3") | password.contains("4") | password.contains("5") | password.contains("6") | password.contains("7") | password.contains("8") | password.contains("9")){
+                passGood = 'y';
+            }
+        }
+
+        //validate if password contains at least one capital letter
+        for (int i=0; i < password.length(); i++){
+            if(Character.isUpperCase(password.charAt(i))){
+                passGood = 'y';
+            }
+            while (passGood != 'y'){
+                System.out.println("This password does not contain a capital letter. Your password must contain at least one capital letter.\nEnter new password: ");
+                password.scan.next();
+                if(Character.isUpperCase(password.charAt(i))){
+                    passGood = 'y';
+                }
+            }
+        }
 		} // end validation loop		
 		
 		person.add(password);
